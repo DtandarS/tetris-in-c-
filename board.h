0626650c3219066075986f8f62d0416771a4fe7c
DtandarS <tom15naksutti@gmail.com>
2025-06-08 17:42:58 +0300

    little more progress in creating my on tetris in c++

--- board.h
+++ board.h
@@ -6,6 +6,73 @@
 
 /*we include self written shit that i still have no idea what the fuck they do but they look fucking cool*/
 
+#include <pieces.h>
+
+
+/* Defines */
+
+#define BOARD_LINE_WIDTH 6 
+#define BLOCK_SIZE 14
+#define BOARD_POSITION 
+#define BOARD_WIDTH 40
+#define BOARD_HEIGHT 20
+#define MIN_HORIZONTAL_MARGIN 10
+#define MIN_VERTICAL_MARGIN 10
+#define PIECE_BLOCK 5
+
+
+/* A class that configures the functionalitites for blocks position and check collisions */
+
+class Board {
+
+  public: 
+    Board (pieces *PieceToDraw, int ScreenHeight);
+
+
+    int GetPositionInPixelX (int Position);
+    int GetPositionInPixelY (int Position);
+    bool isFreeBlock (int positionX, int positionY);
+    bool isPossibleMovement (int positionX, int positionY, int PieceToDraw, int PossibleRotations);
+    void storePiece (int positionX, int positionY, int PieceToDraw, int PossibleRotations);
+    void deletePossibleLines ();
+    bool gameOver ();
+
+  private: 
+    enum {POS_FREE, POS_FILLED} // POS_FREE frees out the position on the board, POS_FILLED fills out the position on the board
+    int mBoard {BOARD_WIDTH, BOARD_HEIGHT}
+    int mScreenHeight;
+
+    void InitBoard();
+    void deleteLine(positonY);
+
+};
+
+#endif
+
+
+void BOARD::InitBoard () {
+
+  for( int i = 0; i < BOARD_WIDTH; i++)
+  for( int j = 0; j < BOARD_HEIGHT; j++)
+
+    mBoard[i][j] = POS_FREE
+
+};
+
+
+void BOARD::storePiece (int positionX, int positionY, int PieceToDraw, int PossibleRotations){
+
+  // Store each block of the piece into the board
+  for (int i1 = pX, i2 = 0; i1 < pX + PIECE_BLOCK; i1++, i2++)
+  {
+    for (int j1 = pY, j2 = 0; j1 < pY + PIECE_BLOCK; j1++, j2++)
+    {
+      // Store only the blocks of the piece that are not holes
+      if (mPieces->GetBlockType (PieceToDraw, PossibleRotations, j2, i2) != 0)
+        mBoard[i1][j1] = POS_FILLED;
+    }
+  }
+}
 
 
 
--- pieces.h
+++ pieces.h
@@ -12,15 +12,15 @@ class Pieces {
 
   public: 
     int GetBlockType(int PieceToDraw, int PossibleRotations, int positioX, int positioY);
-    int InitialPosX(int PieceToDraw, int PossibleRotations);
-    int InitialPosY(int PieceToDraw, int PossibleRotations);
+    int HorizontalInitalPositon(int PieceToDraw, int PossibleRotations);
+    int VerticalInitalPosition(int PieceToDraw, int PossibleRotations);
 
 };
 
 #endif // _PIECES_
 
 /*
-======================================
+ 
 Return the type of a block (0 = no-block, 1 = normal block, 2 = pivot block)
 
 Parameters:
@@ -29,7 +29,7 @@ Parameters:
 >> PossibleRotations: 1 of the 4 possible rotations
 >> positionX: Horizontal position in blocks
 >> positionY: Vertical position in blocks
-======================================
+
 */
 
 int Pieces::GetBlockType(int PieceToDraw, int PossibleRotations, int positioX, int positionY){
@@ -39,7 +39,7 @@ int Pieces::GetBlockType(int PieceToDraw, int PossibleRotations, int positioX, i
 }
 
 /*
-======================================
+ 
 Returns the horizontal displacement of the piece that has to be applied in order to create it in the
 correct position.
 
@@ -47,17 +47,17 @@ Parameters:
 
 >> PieceToDraw: Piece to draw
 >> PossibleRotations: 1 of the 4 possible rotations
-======================================
+
 */
 
 int Pieces::HorizontalInitalPositon (int PieceToDraw, int PossibleRotations){
 
-return PiecesInitialPosition [PieceToDraw][PossibleRotations][0];
+return InitialPosition [PieceToDraw][PossibleRotations][0];
 
 }
 
 /*
-======================================
+
 Returns the vertical displacement of the piece that has to be applied in order to create it in the
 correct position.
 
@@ -65,12 +65,12 @@ Parameters:
 
 >> PieceToDraw: Piece to draw
 >> PossibleRotations: 1 of the 4 possible rotations
-======================================
+
 */
 
 int Pieces::VerticalInitalPosition ( int PieceToDraw, int PossibleRotations){ 
 
-  return PiecesInitialPosition [PieceToDraw][PossibleRotations][1];
+  return InitialPosition [PieceToDraw][PossibleRotations][1];
 
 }
 
